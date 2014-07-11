02c2f5de0586a4f375b7baeacccb97efe11f8336
HatsuneMiku <999hatsune@gmail.com>
2014-07-11 17:55:22 +0900

    add module make_GitHub_wiki_tree

---
+++ make_GitHub_wiki_tree/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+'''make_GitHub_wiki_tree
+This is a test module for mkdoc.py
+'''
+
+__version__ = '0.0.0'
+
+def init():
+  '''This is a function'''
+  pass
+
+if __name__ == '__main__':
+  init()
